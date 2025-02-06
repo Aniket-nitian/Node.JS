6808cd42a6c35bc28d38fbb7b8bf5194d155de56
Aniket-nitian <anik807621@gmail.com>
2025-02-07 00:40:35 +0530

    HTTP completed

---
+++ HTTP/server.js
@@ -0,0 +1,22 @@
+const http = require("http");
+
+//? .createServer()
+
+//web server
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.end("Hello I am coming from web server");
+  }
+  if (req.url === "/about") {
+    res.setHeader("Content-Type", "text/plain");
+    res.end("About page, using nodemon.");
+  }
+});
+
+//*Event emitter
+
+//! server call or Emit
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
