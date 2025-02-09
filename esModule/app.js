e845841b1fe0aad257dadfd9a03409df90ecdabe
Aniket-nitian <anik807621@gmail.com>
2025-02-09 10:07:32 +0530

    esModule completed

---
+++ esModule/app.js
@@ -0,0 +1,9 @@
+import multiply from "./math.js";
+import { PI, divide, subs, add } from "./math.js";
+//import * as math from "./math.js";
+
+console.log(add(10, 5));
+console.log(PI);
+console.log(divide(5, 5));
+console.log(multiply(5, 5));
+console.log(subs(5, 5));
---
+++ esModule/math.js
@@ -0,0 +1,19 @@
+export const add = (a, b) => {
+  return a + b;
+};
+
+export const subs = (a, b) => {
+  return a - b;
+};
+
+export const multiply = (a, b) => {
+  return a * b;
+};
+
+export const divide = (a, b) => {
+  return a / b;
+};
+
+export const PI = 3.213;
+
+export default multiply;
